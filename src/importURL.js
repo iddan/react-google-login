7558d5588bde3a7aab5c4692a7f20280d8497944
Iddan Aaronsohn <mail@aniddan.com>
2018-07-07 17:50:46 +0300

    Fixed flow

--- src/importURL.js
+++ src/importURL.js
@@ -4,11 +4,12 @@
  * Imports module by URL
  * @param url URL of module to be imported
  */
-export default function importURL(url: string): Promise<undefined> {
+export default function importURL(url: string): Promise<typeof undefined> {
   return new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.setAttribute("module", true);
+    script.setAttribute("module", "true");
     script.src = url;
+    // $FlowFixMe
     document.body.appendChild(script);
     script.addEventListener("load", () => {
       resolve();
